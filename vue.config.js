36e4cde75773f6186bd8b986e7a17d7aec720287
Ren Baoshuo <i@baoshuo.ren>
2021-07-19 21:37:02 +0800

    Add title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = '做题记录'
+        return args
+      })
+  }
 }
